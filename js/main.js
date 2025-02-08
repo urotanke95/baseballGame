128769573893b540e3951010c969d88535615263
Kentaro Ino <urotankei3t4@hvrl.ics.keio.ac.jp>
2025-02-08 11:51:02 +0900

    カメラ位置調整

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const engine = new BABYLON.Engine(canvas, true);
 const createScene = function () {
     const scene = new BABYLON.Scene(engine);
     
-    const camera = new BABYLON.ArcRotateCamera("camera", Math.PI / 4, Math.PI / 4, 20, BABYLON.Vector3.Zero(), scene);
+    const camera = new BABYLON.ArcRotateCamera("camera", 0, Math.PI / 3, 20, BABYLON.Vector3.Zero(), scene);
     camera.attachControl(canvas, true);
     
     const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
